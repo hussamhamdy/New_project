10c5193e79c75a118b5f5dafccc4f7c18deb51d8
hussamhamdy <72318178+hussamhamdy@users.noreply.github.com>
2022-02-16 21:31:31 +0200

    System 1.0.0
    tim4 count down
    mcp 1st draft

--- Systems/Core/Src/main.c
+++ Systems/Core/Src/main.c
@@ -411,7 +411,7 @@ static void MX_TIM4_Init(void)
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 7999;
-  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_DOWN;
   htim4.Init.Period = 65535;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
