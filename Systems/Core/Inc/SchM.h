989ec6c1f99c5868862d4bbd3a1f7845cee334a4
Mahmoud Ayman <70566401+Mahmoud-Ayman-Fahmy@users.noreply.github.com>
2022-02-16 21:05:57 +0200

    System 1.0.0
    Create SchM

---
+++ Systems/Core/Inc/SchM.h
@@ -0,0 +1,23 @@
+/*
+ * SchM.h
+ *
+ *  Created on: Feb 13, 2022
+ *      Author: Mahmoud Ayman
+ */
+
+
+#ifndef SCHM_H_
+#define SCHM_H_
+
+
+
+#define SCHM_SYSTEM_TICK	5
+
+extern void SchM_vidScheduler (void);
+extern void SchM_vidInit (void);
+
+
+#endif /* SCHM_H_ */
+
+
+
